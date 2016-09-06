342ac21fcef33703e4d00ba5dd45f2364b0a8c2b
zhangzheng <18601254750.163.com>
2016-09-06 14:29:58 +0800

    自动重构引起的修改

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,20 +5,20 @@
 describe('my app', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /status when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/status");
   });
 
 
-  describe('view1', function() {
+  describe('status', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view1');
+      browser.get('index.html#!/status');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render status when user navigates to /status', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
@@ -26,14 +26,14 @@ describe('my app', function() {
   });
 
 
-  describe('view2', function() {
+  describe('cluster', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view2');
+      browser.get('index.html#!/cluster');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
+    it('should render cluster when user navigates to /cluster', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 2/);
     });
