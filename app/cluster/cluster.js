5defbced19baa83e94e85ff1d399092e5eb41cda
zhangzheng <18601254750.163.com>
2016-09-22 15:18:09 +0800

    改进cluster页面

--- app/cluster/cluster.js
+++ app/cluster/cluster.js
@@ -2,21 +2,52 @@
 
 angular.module('myApp.cluster', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/cluster', {
-    templateUrl: 'cluster/cluster.html',
-    controller: 'ClusterCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/cluster', {
+            templateUrl: 'cluster/cluster.html',
+            controller: 'ClusterCtrl'
+        });
+    }])
 
-.controller('ClusterCtrl', ['$scope', '$http', function($scope, $http) {
-  console.log("enter ClusterCtrl");
-  $http({
-    method: 'GET',
-    url: $scope.rootUrl + '/cluster'
-  }).success(function(data, status, headers, config){
-    $scope.cluster = data;
-  }).error(function(data,status,headers,config){
-    console.log(status);
-  });
-}]);
+    .controller('ClusterCtrl', ['$scope', '$http', 'ngNotify', function ($scope, $http, ngNotify) {
+        console.log("enter ClusterCtrl");
+
+        $scope.newNode = "";
+
+        $scope.update = function () {
+            $http({
+                method: 'GET',
+                url: $scope.rootUrl + '/cluster'
+            }).success(function (data, status, headers, config) {
+                $scope.cluster = data;
+            }).error(function (data, status, headers, config) {
+                console.log(status);
+            });
+        }
+
+        $scope.forceLeave = function (val) {
+            $http({
+                method: 'DELETE',
+                url: $scope.rootUrl + '/cluster',
+                params:{name:val},
+            }).success(function (body, status, headers, config) {
+                ngNotify.set("delete cluster ok");
+            }).error(function (body, status, headers, config) {
+                ngNotify.set("delete cluster failed");
+            });
+        }
+
+        $scope.add = function () {
+            $http({
+                method: 'POST',
+                url: $scope.rootUrl + '/cluster',
+                data:{address: $scope.newNode},
+            }).success(function (body, status, headers, config) {
+                ngNotify.set("add cluster ok");
+            }).error(function (body, status, headers, config) {
+                ngNotify.set("add cluster failed");
+            });
+        }
+
+        $scope.update();
+    }]);
