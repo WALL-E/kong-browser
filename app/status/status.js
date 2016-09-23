91d614574ac16449493f6bcebb4db94950ee5210
zhangzheng <18601254750.163.com>
2016-09-23 18:35:50 +0800

    改进代码可读性

--- app/status/status.js
+++ app/status/status.js
@@ -2,21 +2,26 @@
 
 angular.module('myApp.status', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/status', {
-    templateUrl: 'status/status.html',
-    controller: 'StatusCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/status', {
+            templateUrl: 'status/status.html',
+            controller: 'StatusCtrl'
+        });
+    }])
 
-.controller('StatusCtrl', ['$scope', '$http', function($scope, $http) {
-  console.log("enter StatusCtrl");
-  $http({
-    method: 'GET',
-    url: $scope.rootUrl + '/status'
-  }).success(function(data, status, headers, config){
-    $scope.status = data;
-  }).error(function(data,status,headers,config){
-    console.log(status);
-  });
-}]);
+    .controller('StatusCtrl', ['$scope', '$http', function ($scope, $http) {
+        console.log("enter StatusCtrl");
+
+        $scope.update = function () {
+            $http({
+                method: 'GET',
+                url: $scope.rootUrl + '/status'
+            }).success(function (body, status, headers, config) {
+                $scope.status = body;
+            }).error(function (data, status, headers, config) {
+                console.log(status);
+            });
+        }
+
+        $scope.update();
+    }]);
